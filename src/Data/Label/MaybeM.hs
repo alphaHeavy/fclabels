d417fa8055d2d995a07721444d0ee9d897057b89
Steve Severance <sseverance@alphaheavy.com>
2012-05-01 20:11:25 -0700

    renaming

--- src/Data/Label/MaybeM.hs
+++ src/Data/Label/MaybeM.hs
@@ -4,7 +4,8 @@ module Data.Label.MaybeM
 (
 -- * 'MonadState' lens operations.
   gets
-, putsMaybe
+, puts
+, putsWith
 -- * 'MonadReader' lens operations.
 , asks
 )
@@ -23,8 +24,8 @@ import qualified Data.Label.Maybe     as L
 gets :: (M.MonadState f m, MonadPlus m) => (f :~> a) -> m a
 gets l = (L.get l `liftM` M.get) >>= (mzero `maybe` return)
 
-putsMaybe :: forall a m s. M.MonadState s m => (s :~> a) -> a -> m ()
-putsMaybe yourLens value = do
+puts :: forall a m s. M.MonadState s m => (s :~> a) -> a -> m ()
+puts yourLens value = do
     M.modify (doSet yourLens value)
   where
     doSet :: forall f a. (f :~> a) -> a -> f -> f
@@ -33,6 +34,13 @@ putsMaybe yourLens value = do
       in  case result of
             Just x -> x
             Nothing -> error "putsMaybeM returned nothing"
+
+putsWith :: (M.MonadState f m, MonadPlus m) => (f :~> a) -> (a -> a -> Bool) -> a -> m ()
+putsWith yourLens predicate value = do
+  s <- gets yourLens
+  case s `predicate` value of
+    True -> puts yourLens value
+    False -> return ()
 
 -- | Fetch a value, pointed to by a lens that might fail, out of a reader
 -- environment. When the lens getter fails this computation will fall back to
